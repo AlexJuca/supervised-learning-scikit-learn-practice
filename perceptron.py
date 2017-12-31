e6befb299b8fb586363813ab2c67fb2808f6c546
Alexandre Juca <corextechnologies@gmail.com>
2017-12-31 21:33:22 +0100

    better variable names

--- perceptron.py
+++ perceptron.py
@@ -12,53 +12,39 @@ class Perceptron:
 
     Attributes
     -----------
-    w_ : 1d-array
+    weights : 1d-array
         Weights after fitting.
     errors_ : list
         Number of misclassifications in every epoch
 
     """
-    def __init__(self, alpha=0.01, n_iter=10):
+    def __init__(self, alpha=0.01, n_iterations=10):
         self.alpha = alpha
-        self.n_ter = n_ter
+        self.n_iterations = n_iterations
 
     def fit(self, X, y):
-    """Fit training data.
+        self.weights = np.zeros(1 + X.shape[1])
+        self.errors_ = []
+        for _ in range(self.n_iterations):
+            errors = 0
+            for xi, target in zip(X, y):
+                update = self.alpha * (target - predict(self, xi))
+                self.weights[1:] += update * xi
+                print(self.weights)
+                self.weights[0] += update
+                errors += int(update != 0.0)
+                self.errors_.append(errors)
+                print("--- ERROS ---")
+                print(self.errors_)
+                # if(self.errors_[len(self.errors_) - 1] == 0):
+                 #   break
 
-        Parameters
-        ----------
-        X : {array-like}, shape = [n_samples, n_features]
-            Training vectors, where n_samples is the number of samples and n_features is the number of features.
-
-        y : array-like, shape = [n_samples]
-            Target values. 
-
-        Returns 
-        -------
-        self : object
-
-    """
-
-    self.w_ = np.zeros(1 + X.shape[1])
-    self.errors_ = []
-
-    for _ in range(self.n_iter):
-        errors = 0
-        for xi, in target in zip(X, y):
-            update = self.alpha * (target - self.predict(xi))
-            self.w_[1:] += update * xi
-            self.w_[0] += update
-            errors += int(update != 0.0)
-        self.errors_.append(errors)
-    return self
+        return self
 
 def net_input(self, X):
     """Calculate net input """
-    return np.dot(X, self.w_[1:]) + self.w_[0]
+    return np.dot(X, self.weights[1:]) + self.weights[0]
 
 def predict(self, X):
     """Return the class label after unit step:"""
-    return np.where(self.net_input(X) >= 0.0, 1, -1)
-
-
-
+    return np.where(net_input(self, X) >= 0.0, 1, -1)
